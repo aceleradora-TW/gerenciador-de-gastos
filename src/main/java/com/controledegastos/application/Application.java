ab3a188b95be79914b7224048b832522da253b1f
brendamoniqueb <bmbrocardo@gmail.com>
2019-03-15 15:35:36 -0300

    cria exercicio v1

---
+++ src/main/java/com/controledegastos/application/Application.java
@@ -0,0 +1,49 @@
+package com.controledegastos.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        List<Gasto>gastos = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Selecione a ação: ");
+        System.out.println("1. Adicionar gasto");
+        System.out.println("2. Remover gasto");
+        System.out.println("3. Calcular total de gastos");
+
+        int acao = scanner.nextInt();
+
+        if (acao == 1){
+            System.out.println("Digite o produto e o seu valor");
+            String resposta = scanner.next();
+            gastos.add(getGasto(resposta));
+            System.out.println(gastos.get(0).getDescricao());
+            System.out.println(gastos.get(0).getValor());
+        }
+
+    }
+
+    public static Gasto getGasto(String resposta){
+        String descricao[] = resposta.split(";");
+        Double valor = parseValor(descricao[1]);
+        Gasto gasto = new Gasto(descricao[0], valor);
+        return gasto;
+    }
+
+    public static Double parseValor(String valor){
+        Double valorDouble = Double.parseDouble(valor);
+        return valorDouble;
+    }
+
+
+}
---
+++ src/main/java/com/controledegastos/application/Gasto.java
@@ -0,0 +1,28 @@
+package com.controledegastos.application;
+
+public class Gasto {
+    String descricao;
+    double valor;
+
+    public Gasto(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
+
